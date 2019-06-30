2fbf870b7a9b0dc396fc0b6c590e0001405620fb
Maxim Vasilishin <maxv8199@gmail.com>
2019-06-30 21:21:05 +0300

    init commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+
+export default class TodoList extends Component {
+  render() {
+    const {items,clearList,handleDelete,handleEdit} = this.props;
+    return (
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">Todo List</h3>
+        {
+          items.map(item=>{
+            return(
+              <TodoItem 
+              key={item.id} 
+              title={item.title}
+              handleDelete={()=> handleDelete(item.id)}
+              handleEdit={()=> handleEdit(item.id)}
+              />
+            )
+          })
+        }
+        <button
+          type="submit"
+          className="btn btn-danger btn-block mt-5 text-capitalize"
+          onClick={clearList}
+        >
+          Clear List
+        </button>
+      </ul>
+    );
+  }
+}
